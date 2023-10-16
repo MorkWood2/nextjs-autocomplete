ce40e2fe2d7bb3ebf36e8d6cb31025da3f05c433
Mark Wild <mark.wild189@gmail.com>
2023-10-16 10:11:53 -0700

    chore: autocomplete component logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,39 @@
-'use client'
-import Autocomplete from './components/Autocomplete'
-// import { useState, useEffect } from "react"
+'use client';
+import Autocomplete from './components/Autocomplete';
 
 export default function Home() {
-
-//   const [data, setData] = useState([])
-
-
-//   useEffect(() => {
-// const getData = async () => {
-//   const query = await fetch('https://jsonplaceholder.typicode.com/users')
-//   const response = await query.json()
-//   console.log(response);
-
-//   setData(response)
-// }
-// getData()   
-//   }, [])
-
-
+  const cityNames = [
+    'Amsterdam',
+    'Berlin',
+    'London',
+    'New York',
+    'Paris',
+    'Rome',
+    'San Francisco',
+    'Tokyo',
+    'Washington DC',
+    'Zurich',
+    'Copenhagen',
+    'Helsinki',
+    'Madrid',
+    'Reykjavik',
+    'Stockholm',
+    'Vancouver',
+    'Vienna',
+    'Zagreb',
+    'Budapest',
+    'Dublin',
+    'Hamburg',
+    'Krakow',
+    'Lisbon',
+    'Ljubljana',
+  ];
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <div>
-        <Autocomplete />
+        <Autocomplete options={cityNames} />
       </div>
-
-
-      {/* <div>
-        {
-          data && data.length && data.map((user:any, index: any) => {
-            console.log(index);
-            return (
-              <div key={user.id}>
-                {user.name}
-              </div>
-            )
-            
-          })
-        }
-
-      </div> */}
-
-      
- 
-
     </main>
-  )
+  );
 }
